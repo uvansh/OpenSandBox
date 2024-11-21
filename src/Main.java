ce16dea7789e4ea80df6c5cc2459f052e8b31bc8
Vansh <uuvansh@gmail.com>
2024-11-21 19:45:59 +0530

    Renamed Repository and JDBC set up done

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,24 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 
+        /*
+        Setup-ing connection: Every Connection needs username,password,query
+        url which has connector like jdbc here, host,port and database-name
+        */
         String sql = "select first_name from actor where actor_id = 8";
         String url = "jdbc:postgresql://localhost:5432/dvdrental";
         String username = "postgres";
         String password = "mysql123";
-
-        Connection con = DriverManager.getConnection(url,username,password);
+        //Connection is a session between a Java application and a database. It helps to establish a connection with the database.
+        //The DriverManager class acts as an interface between users and drivers. It keeps track of the drivers that are available and handles establishing a connection between a database and the appropriate driver.
+        //The Statement interface provides methods to execute queries with the database.
+        //
+        Connection con = DriverManager.getConnection(url, username, password);
         Statement st = con.createStatement();
         st.executeQuery(sql);
         ResultSet rs = st.executeQuery(sql);
         rs.next();
-        String name =rs.getString(1);
+        String name = rs.getString(1);
         System.out.println(name);
         con.close();
     }
